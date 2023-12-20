4c948f4084feb8c36dbcec1e59a6a5566ccfdcf6
Emrah Aksoy <emraksoy37@gmail.com>
2023-12-20 22:36:26 +0300

    android versionCode 3

--- app.config.js
+++ app.config.js
@@ -1,20 +1,25 @@
-module.exports = () => {
-  if (process.env.MY_ENVIRONMENT === "production") {
-    return {
-      name: "Cryptem",
-      slug: "cryptem",
-      version: "1.0.0",
-      sdkVersion: "49.0.0",
-      platforms: ["ios", "android"],
-      extra: {
-        eas: {
-          projectId: "6aa4e65c-0d42-462d-a960-bd7592c39419",
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    android: {
+      versionCode: 3,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#000000",
+      },
+      package: "com.emraksoy.cyrptem",
+      runtimeVersion: "1.0.0",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_API_KEY,
         },
       },
-    };
-  } else {
-    return {
-      /* your development config */
-    };
-  }
+    },
+
+    extra: {
+      eas: {
+        projectId: "6aa4e65c-0d42-462d-a960-bd7592c39419",
+      },
+    },
+  };
 };
